6be80e8a606d7df97efa94924cf32ef8c661489d
= <rabesonmamitiana@gmail.com>
2024-01-24 21:41:44 +0300

    source_code design test

---
+++ test/code.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1.25.0/themes/prism.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/prismjs@1.25.0"></script>
+</head>
+<body>
+    <pre>
+        <code class="language-javascript" style="width:100px;">
+            Console.Write("Hello, World");
+        </code>
+    </pre>
+
+</body>
+</html>
